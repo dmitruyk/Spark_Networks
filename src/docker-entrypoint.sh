a1cb5b1901f49459b5031f384f51321c21b45612
root <root@ip-172-31-32-26.eu-central-1.compute.internal>
2019-03-22 15:10:04 +0000

    fix

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -4,4 +4,4 @@ until python manage.py migrate --no-input; do
   >&2 echo "Postgres database is unavailable - sleeping"
   sleep 1
 done
-gunicorn bookstore.wsgi:application --workers $DEASY_GUNICORN_NUMBER_OF_WORKERS --bind :8000
+gunicorn test_task.wsgi:application --workers $DEASY_GUNICORN_NUMBER_OF_WORKERS --bind :8000
