82eadbe9715be5b8a7b450994f307a551a2812c5
dmitruyk <dmitruyk@gmail.com>
2019-03-22 19:51:31 +0200

    company & contact added to load view

--- src/rentdata/models/general.py
+++ src/rentdata/models/general.py
@@ -3,6 +3,12 @@ from django.db import models
 
 from django.utils.translation import ugettext_lazy as _
 
+class Status(CoreModel):
+    class Meta:
+        db_table = u'status'
+
+    status = models.BooleanField(default=False),
+
 
 class Company(CoreModel):
     class Meta:
@@ -28,13 +34,13 @@ class Company(CoreModel):
     contactDetails_cellPhoneNumberAreaCode = models.PositiveIntegerField(_('contactDetails_address_postcode'),\
                                                                      null=True, blank=True, default=0,\
                                                                      unique=False)
-    contactDetails_cellPhoneNumberCountryCode = models.CharField(_('contactDetails_cellPhoneNumber'), max_length=10, default=None)
+    contactDetails_cellPhoneNumberCountryCode = models.CharField(_('contactDetails_cellPhoneNumber'), max_length=100, default=None)
 
-    contactDetails_cellPhoneNumberSubscribe = models.CharField(_('contactDetails_cellPhoneNumber'), max_length=20, default=None)
+    contactDetails_cellPhoneNumberSubscriber = models.CharField(_('contactDetails_cellPhoneNumber'), max_length=20, default=None)
 
     contactDetails_company = models.CharField(_('contactDetails_cellPhoneNumber'), max_length=180, default=None)
 
-    contactDetails_countryCode = models.CharField(_('contactDetails_cellPhoneNumber'), max_length=10, default=None)
+    contactDetails_countryCode = models.CharField(_('contactDetails_cellPhoneNumber'), max_length=100, default=None)
 
     contactDetails_email = models.CharField(_('contactDetails_email'), max_length=50, default=None)
 
@@ -42,9 +48,9 @@ class Company(CoreModel):
 
     contactDetails_faxNumberAreaCode = models.CharField(_('contactDetails_faxNumberAreaCode'), max_length=10, default=None)
 
-    contactDetails_faxNumberCountryCode = models.CharField(_('contactDetails_faxNumberCountryCode'), max_length=10, default=None)
+    contactDetails_faxNumberCountryCode = models.CharField(_('contactDetails_faxNumberCountryCode'), max_length=100, default=None)
 
-    contactDetails_faxNumberSubscriber = models.CharField(_('contactDetails_faxNumberSubscriber'), max_length=10, default=None)
+    contactDetails_faxNumberSubscriber = models.CharField(_('contactDetails_faxNumberSubscriber'), max_length=100, default=None)
 
     contactDetails_firstname = models.CharField(_('contactDetails_firstname'), max_length=30, default=None)
 
@@ -60,23 +66,23 @@ class Company(CoreModel):
 
     contactDetails_phoneNumberSubscriber = models.CharField(_('contactDetails_phoneNumberSubscriber'), max_length=20, default=None)
 
-    contactDetails_salutation = models.CharField(_('contactDetails_salutation'), max_length=10, default=None)
+    contactDetails_salutation = models.CharField(_('contactDetails_salutation'), max_length=100, default=None)
 
-    contactFormConfiguration_addressField = models.CharField(_('contactFormConfiguration_addressField'), max_length=10, default=None)
+    contactFormConfiguration_addressField = models.CharField(_('contactFormConfiguration_addressField'), max_length=100, default=None)
 
     contactFormConfiguration_applicationPackageCompletedField = models.CharField(_('contactFormConfiguration_applicationPackageCompletedField'), max_length=10, default=None)
 
-    contactFormConfiguration_emailAddressField = models.CharField(_('contactFormConfiguration_emailAddressField'), max_length=10, default=None)
+    contactFormConfiguration_emailAddressField = models.CharField(_('contactFormConfiguration_emailAddressField'), max_length=100, default=None)
 
     contactFormConfiguration_employmentRelationshipField = models.CharField(_('contactFormConfiguration_employmentRelationshipField'), max_length=10, default=None)
 
-    contactFormConfiguration_firstnameField = models.CharField(_('contactFormConfiguration_firstnameField'), max_length=10, default=None)
+    contactFormConfiguration_firstnameField = models.CharField(_('contactFormConfiguration_firstnameField'), max_length=100, default=None)
 
-    contactFormConfiguration_freemiumSettings_duratio = models.PositiveIntegerField(default=0)
+    contactFormConfiguration_freemiumSettings_duration = models.PositiveIntegerField(default=0)
 
-    contactFormConfiguration_incomeField = models.CharField(_('contactFormConfiguration_incomeField'), max_length=10, default=None)
+    contactFormConfiguration_incomeField = models.CharField(_('contactFormConfiguration_incomeField'), max_length=100, default=None)
 
-    contactFormConfiguration_lastnameField = models.CharField(_('contactFormConfiguration_lastnameField'), max_length=10, default=None)
+    contactFormConfiguration_lastnameField = models.CharField(_('contactFormConfiguration_lastnameField'), max_length=100, default=None)
 
     contactFormConfiguration_messageField = models.CharField(_('contactFormConfiguration_messageField'), max_length=40, default=None)
 
@@ -91,6 +97,10 @@ class Company(CoreModel):
     contactFormConfiguration_premiumProfileRequired = models.CharField(_('contactFormConfiguration_premiumProfileRequired'), max_length=40, default=None)
 
     contactFormConfiguration_salutationField = models.CharField(_('contactFormConfiguration_salutationField'), max_length=40, default=None)
+
+    def save(self, *args, **kwargs):
+        super(Company, self).save(*args, **kwargs)
+        return self.id
 
 
 class ImprintLink(CoreModel):
@@ -120,6 +130,8 @@ class Apartment(CoreModel):
 
     contact_form_type = models.CharField(_('contactFormType'), max_length=80)
 
+    remove_creation = models.DateTimeField(null=True, blank=True)
+
     remove_id = models.PositiveIntegerField(_('id'), null=True, blank=True, default=0, unique=False)
 
     remove_modification = models.DateTimeField(null=True, blank=True)
@@ -131,6 +143,10 @@ class Apartment(CoreModel):
     adLinkForJSONP_xlink_href = models.TextField(_('adLinkForJSONP_xlink_href'), default=None)
 
     adLinkForXMLData_xlink_href = models.TextField(_('adLinkForXMLData_xlink_href'), default=None)
+
+    def save(self, *args, **kwargs):
+        super(Apartment, self).save(*args, **kwargs)
+        return self.id
 
 
 class RealEstate(CoreModel):
@@ -181,18 +197,196 @@ class RealEstate(CoreModel):
     x_link_href = models.CharField(_('x_link_href'), max_length=280, default=None)
 
 
-# class RealEstateAttachments(CoreModel):
-#     class Meta:
-#         db_table = u'realestate_attachments'
-#     apartment = models.ForeignKey(
-#         RealEstate,
-#         related_name='real_estate_attachments',
-#         on_delete=models.SET_NULL,
-#         null=True, blank=True
-#     )
-#
-#     xsi_type =
+class RealEstateAttachments(CoreModel):
+    class Meta:
+        db_table = u'realestate_attachments'
+    apartment = models.ForeignKey(
+        RealEstate,
+        related_name='real_estate_attachments',
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+
+    xsi_type = models.CharField(_('xsi_type'), max_length=150, default=None),
+
+    xlink_href = models.CharField(_('xlink_href'), max_length=250, default=None),
+
+    id = models.CharField(_('id'), max_length=250, default=None),
+
+    modification = models.DateTimeField(null=True, blank=True)
+
+    creation = models.DateTimeField(null=True, blank=True)
+
+    publishDate = models.DateTimeField(null=True, blank=True)
+
+    title = models.CharField(_('title'), max_length=250, default=None)
+
+    floorplan = models.BooleanField(default=False),
+
+    titlepicture = models.BooleanField(default=False),
+
+    realEstate_balcony = models.BooleanField(default=False),
+
+    real_estate_baseRent = models.CharField(_('real_estate_baseRent'), max_length=250, default=None),
+
+    real_estate_building_energy_rating_type = models.CharField(_('real_estate_building_energy_rating_type'), max_length=250, default=None),
+
+    real_estate_built_in_kitchen = models.BooleanField(default=False),
+
+    real_estate_calculated_total_rent = models.CharField(_('real_estate_calculated_total_rent'), max_length=250, default=None),
+
+    real_estate_calculated_total_rent_scope = models.CharField(_('real_estate_calculated_total_rent_scope'), max_length=250, default=None),
+
+    real_estate_cellar = models.CharField(_('real_estate_cellar'), max_length=250, default=None),
+
+    real_estate_certificate_of_eligibility_needed = models.CharField(_('real_estate_certificate_of_eligibility_needed'), max_length=250, default=None),
+
+    real_estate_condition = models.CharField(_('real_estate_condition'), max_length=250, default=None),
+
+    real_estate_construction_year = models.PositiveIntegerField(_('real_estate_construction_year'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False)
+
+    realEstate_courtage_hasCourtage = models.CharField(_('realEstate_courtage_hasCourtage'), max_length=250, default=None),
+
+    realEstate_creationDate = models.DateTimeField(null=True, blank=True),
+
+    realEstate_deposit = models.CharField(_('realEstate_deposit'), max_length=250, default=None),
+
+    realEstate_descriptionNote = models.CharField(_('realEstate_descriptionNote'), max_length=1250, default=None),
+
+    realEstate_energyCertificate_energyCertificateAvailability = models.CharField(_('realEstate_energyCertificate_energyCertificateAvailability'), max_length=250, default=None),
+
+    realEstate_energyCertificate_energyCertificateCreationDate = models.CharField(_('realEstate_energyCertificate_energyCertificateCreationDate'), max_length=1250, default=None),
+
+    realEstate_energyConsumptionContainsWarmWater = models.CharField(_('realEstate_energyConsumptionContainsWarmWater'), max_length=250, default=None),
+
+    realEstate_energyPerformanceCertificate = models.BooleanField(default=False),
+
+    realEstate_externalId = models.PositiveIntegerField(_('realEstate_externalId'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False)
+
+    realEstate_floor = models.PositiveIntegerField(_('realEstate_floor'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False)
+
+    realEstate_floorplan = models.BooleanField(default=False),
+
+    realEstate_freeFrom = models.CharField(_('realEstate_freeFrom'), max_length=250, default=None),
+
+    realEstate_furnishingNote = models.CharField(_('realEstate_furnishingNote'), max_length=1250, default=None),
+
+    realEstate_garden = models.BooleanField(default=False),
+
+    realEstate_guestToilet = models.CharField(_('realEstate_guestToilet'), max_length=50, default=None),
+
+    realEstate_handicappedAccessible = models.CharField(_('realEstate_handicappedAccessible'), max_length=50, default=None),
+
+    realEstate_heatingCosts = models.PositiveIntegerField(_('realEstate_heatingCosts'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False),
+
+    realEstate_heatingCostsInServiceCharge = models.CharField(_('realEstate_heatingCostsInServiceCharge'), max_length=50, default=None),
+
+    realEstate_heatingType = models.CharField(_('realEstate_heatingType'), max_length=50, default=None),
+
+    realEstate_heatingTypeEnev2014 = models.CharField(_('realEstate_heatingTypeEnev2014'), max_length=50, default=None),
+
+    realEstate_interiorQuality = models.CharField(_('realEstate_interiorQuality'), max_length=50, default=None),
+
+    realEstate_lastModificationDate = models.DateTimeField(null=True, blank=True),
+
+    realEstate_lastRefurbishment = models.PositiveIntegerField(_('realEstate_lastRefurbishment'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False),
+
+    realEstate_lift = models.BooleanField(default=False),
+
+    realEstate_livingSpace = models.PositiveIntegerField(_('realEstate_livingSpace'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False),
+
+    realEstate_locationNote = models.CharField(_('realEstate_locationNote'), max_length=1250, default=None),
+
+    realEstate_numberOfFloors = models.IntegerField(_('realEstate_numberOfFloors'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False),
+
+    realEstate_numberOfRooms = models.PositiveIntegerField(_('realEstate_numberOfRooms'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False),
+
+    realEstate_otherNote = models.CharField(_('realEstate_otherNote'), max_length=1250, default=None),
+
+    realEstate_petsAllowed = models.CharField(_('realEstate_petsAllowed'), max_length=50, default=None),
+
+    realEstate_referencePriceApiCall = models.CharField(_('realEstate_referencePriceApiCall'), max_length=150, default=None),
+
+    realEstate_referencePriceServiceCall = models.CharField(_('realEstate_referencePriceServiceCall'), max_length=150, default=None),
+
+    realEstate_serviceCharge = models.IntegerField(_('realEstate_numberOfFloors'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False),
+
+    realEstate_state = models.CharField(_('realEstate_referencePriceServiceCall'), max_length=150, default=None),
+
+    realEstate_thermalCharacteristic = models.CharField(_('realEstate_thermalCharacteristic'), max_length=50, default=None),
+
+    realEstate_title = models.CharField(_('realEstate_thermalCharacteristic'), max_length=250, default=None),
+
+    realEstate_titlePicture_creation = models.DateTimeField(null=True, blank=True),
+
+    realEstate_titlePicture_id = models.IntegerField(_('realEstate_numberOfFloors'),\
+                                                                     null=True, blank=True, default=0,\
+                                                                     unique=False),
+
+    realEstate_titlePicture_modification = models.DateTimeField(null=True, blank=True),
+
+    realEstate_titlePicture_publishDate = models.DateTimeField(null=True, blank=True),
+
+    realEstate_titlePicture_floorplan = models.BooleanField(default=False),
+
+    realEstate_titlePicture_title = models.CharField(_('realEstate_titlePicture_title'), max_length=250, default=None),
+
+    realEstate_titlePicture_titlePicture = models.BooleanField(default=False),
+
+    realEstate_totalRent = models.CharField(_('realEstate_totalRent'), max_length=250, default=None),
+
+    realEstate_useAsFlatshareRoom = models.CharField(_('realEstate_useAsFlatshareRoom'), max_length=250, default=None),
+
+    realtorValuationJSONLink_xlink_href = models.CharField(_('realtorValuationJSONLink_xlink_href'), max_length=550, default=None),
+
+    realtorValuationV2JSONLink_xlink_href = models.CharField(_('realtorValuationJSONLink_xlink_href'), max_length=550, default=None),
+
+    realtorValuationV2JSONPLink_xlink_href = models.CharField(_('realtorValuationV2JSONPLink_xlink_href'), max_length=550, default=None),
 
 
+class Url(CoreModel):
+    class Meta:
+        db_table = u'url'
+    apartment = models.ForeignKey(
+        RealEstateAttachments,
+        related_name='real_url',
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+
+    scale = models.CharField(_('scale'), max_length=250, default=None),
+
+    href = models.CharField(_('href'), max_length=250, default=None),
 
 
+class RealEstateTitlePictureUrls(CoreModel):
+    class Meta:
+        db_table = u'RealEstateTitlePictureUrls'
+    apartment = models.ForeignKey(
+        RealEstateAttachments,
+        related_name='Real_EstateTitlePictureUrls',
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+
+    scale = models.CharField(_('scale'), max_length=250, default=None),
+
+    href = models.CharField(_('href'), max_length=250, default=None),
--- src/rentdata/views/v1/load_data.py
+++ src/rentdata/views/v1/load_data.py
@@ -1,13 +1,127 @@
 from django.conf import settings
 from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse, HttpResponseForbidden
 import json
-
+from ...models.general import *
 
 def load_data(request):
     if request.method == 'GET':
         with open('rentdata/1.json', 'r', encoding='utf-8') as fh:  # открываем файл на чтение
             data = json.load(fh)
-            return JsonResponse(data, status=400)
+
+            companyWideCustomerId = data['data']['companyWideCustomerId']
+            contactDetails_id = data['data']['contactDetails_id']
+            contactDetails_address_city = data['data']['contactDetails_address_city']
+            contactDetails_address_houseNumber = data['data']['contactDetails_address_houseNumber']
+            contactDetails_address_postcode = data['data']['contactDetails_address_postcode']
+            contactDetails_address_street = data['data']['contactDetails_address_street']
+            contactDetails_cellPhoneNumber = data['data']['contactDetails_cellPhoneNumber']
+            contactDetails_cellPhoneNumberAreaCode = data['data']['contactDetails_cellPhoneNumberAreaCode']
+            contactDetails_cellPhoneNumberCountryCode = data['data']['contactDetails_cellPhoneNumberCountryCode']
+            contactDetails_cellPhoneNumberSubscriber = data['data']['contactDetails_cellPhoneNumberSubscriber']
+            contactDetails_company = data['data']['contactDetails_company']
+            contactDetails_countryCode = data['data']['contactDetails_countryCode']
+            contactDetails_email = data['data']['contactDetails_email']
+            contactDetails_faxNumber = data['data']['contactDetails_faxNumber']
+            contactDetails_faxNumberAreaCode = data['data']['contactDetails_faxNumberAreaCode']
+            contactDetails_faxNumberCountryCode = data['data']['contactDetails_faxNumberCountryCode']
+            contactDetails_faxNumberSubscriber = data['data']['contactDetails_faxNumberSubscriber']
+            contactDetails_firstname = data['data']['contactDetails_firstname']
+            contactDetails_homepageUrl = data['data']['contactDetails_homepageUrl']
+            contactDetails_lastname = data['data']['contactDetails_lastname']
+            contactDetails_phoneNumber = data['data']['contactDetails_phoneNumber']
+            contactDetails_phoneNumberAreaCode = data['data']['contactDetails_phoneNumberAreaCode']
+            contactDetails_phoneNumberCountryCode = data['data']['contactDetails_phoneNumberCountryCode']
+            contactDetails_phoneNumberSubscriber = data['data']['contactDetails_phoneNumberSubscriber']
+            contactDetails_salutation = data['data']['contactDetails_salutation']
+            contactFormConfiguration_addressField = data['data']['contactFormConfiguration_addressField']
+            contactFormConfiguration_applicationPackageCompletedField = data['data']['contactFormConfiguration_applicationPackageCompletedField']
+            contactFormConfiguration_emailAddressField = data['data']['contactFormConfiguration_emailAddressField']
+            contactFormConfiguration_employmentRelationshipField = data['data']['contactFormConfiguration_employmentRelationshipField']
+            contactFormConfiguration_firstnameField = data['data']['contactFormConfiguration_firstnameField']
+            contactFormConfiguration_freemiumSettings_duration = data['data']['contactFormConfiguration_freemiumSettings_duration']
+            contactFormConfiguration_incomeField = data['data']['contactFormConfiguration_incomeField']
+            contactFormConfiguration_lastnameField = data['data']['contactFormConfiguration_lastnameField']
+            contactFormConfiguration_messageField = data['data']['contactFormConfiguration_messageField']
+            contactFormConfiguration_moveInDateField = data['data']['contactFormConfiguration_moveInDateField']
+            contactFormConfiguration_numberOfPersonsField = data['data']['contactFormConfiguration_numberOfPersonsField']
+            contactFormConfiguration_petsInHouseholdField = data['data']['contactFormConfiguration_petsInHouseholdField']
+            contactFormConfiguration_phoneNumberField = data['data']['contactFormConfiguration_phoneNumberField']
+            contactFormConfiguration_premiumProfileRequired = data['data']['contactFormConfiguration_premiumProfileRequired']
+            contactFormConfiguration_salutationField =  data['data']['contactFormConfiguration_salutationField']
+
+
+
+
+
+
+
+
+#Company
+            company = Company(companyWideCustomerId=companyWideCustomerId,
+                             contactDetails_id=contactDetails_id,
+                             contactDetails_address_city=contactDetails_address_city,
+                             contactDetails_address_houseNumber=contactDetails_address_houseNumber,
+                             contactDetails_address_postcode=contactDetails_address_postcode,
+                             contactDetails_address_street=contactDetails_address_street,
+                             contactDetails_cellPhoneNumber=contactDetails_cellPhoneNumber,
+                             contactDetails_cellPhoneNumberAreaCode=contactDetails_cellPhoneNumberAreaCode,
+                             contactDetails_cellPhoneNumberCountryCode=contactDetails_cellPhoneNumberCountryCode,
+                             contactDetails_cellPhoneNumberSubscriber=contactDetails_cellPhoneNumberSubscriber,
+                             contactDetails_company=contactDetails_company,
+                             contactDetails_countryCode=contactDetails_countryCode,
+                             contactDetails_email=contactDetails_email,
+                             contactDetails_faxNumber=contactDetails_faxNumber,
+                             contactDetails_faxNumberAreaCode=contactDetails_faxNumberAreaCode,
+                             contactDetails_faxNumberCountryCode=contactDetails_faxNumberCountryCode,
+                             contactDetails_faxNumberSubscriber=contactDetails_faxNumberSubscriber,
+                             contactDetails_firstname=contactDetails_firstname,
+                             contactDetails_homepageUrl=contactDetails_homepageUrl,
+                             contactDetails_lastname=contactDetails_lastname,
+                             contactDetails_phoneNumber=contactDetails_phoneNumber,
+                             contactDetails_phoneNumberAreaCode=contactDetails_phoneNumberAreaCode,
+                             contactDetails_phoneNumberCountryCode=contactDetails_phoneNumberCountryCode,
+                             contactDetails_phoneNumberSubscriber=contactDetails_phoneNumberSubscriber,
+                             contactDetails_salutation=contactDetails_salutation,
+                             contactFormConfiguration_addressField=contactFormConfiguration_addressField,
+                             contactFormConfiguration_applicationPackageCompletedField=contactFormConfiguration_applicationPackageCompletedField,
+                             contactFormConfiguration_emailAddressField=contactFormConfiguration_emailAddressField,
+                             contactFormConfiguration_employmentRelationshipField=contactFormConfiguration_employmentRelationshipField,
+                             contactFormConfiguration_firstnameField=contactFormConfiguration_firstnameField,
+                             contactFormConfiguration_freemiumSettings_duration=contactFormConfiguration_freemiumSettings_duration,
+                             contactFormConfiguration_incomeField=contactFormConfiguration_incomeField,
+                             contactFormConfiguration_lastnameField=contactFormConfiguration_lastnameField,
+                             contactFormConfiguration_messageField=contactFormConfiguration_messageField,
+                             contactFormConfiguration_moveInDateField=contactFormConfiguration_moveInDateField,
+                             contactFormConfiguration_numberOfPersonsField=contactFormConfiguration_numberOfPersonsField,
+                             contactFormConfiguration_petsInHouseholdField=contactFormConfiguration_petsInHouseholdField,
+                             contactFormConfiguration_phoneNumberField=contactFormConfiguration_phoneNumberField,
+                             contactFormConfiguration_premiumProfileRequired=contactFormConfiguration_premiumProfileRequired,
+                             contactFormConfiguration_salutationField=contactFormConfiguration_salutationField
+                             )
+
+            company_id = company.save()
+            company_related = Company.objects.filter(id=company_id).first()
+
+#First section
+            contact_form_type = data['data']['contactFormType']
+            remove_creation = data['data']['creation']
+            remove_id = data['data']['id']
+            remove_modification = data['data']['modification']
+            remove_publishDate = data['data']['publishDate']
+            xlink_href = data['data']['xlink_href']
+            adLinkForJSONP_xlink_href = data['data']['adLinkForJSONP_xlink_href']
+            adLinkForXMLData_xlink_href = data['data']['adLinkForXMLData_xlink_href']
+
+            apartment = Apartment(company=company_related, contact_form_type=contact_form_type,
+                                  remove_creation=remove_creation, remove_id=remove_id,
+                                  remove_modification=remove_modification,
+                                  remove_publishDate=remove_publishDate,
+                                  xlink_href=xlink_href,
+                                  adLinkForJSONP_xlink_href=adLinkForJSONP_xlink_href,
+                                  adLinkForXMLData_xlink_href=adLinkForXMLData_xlink_href)
+            self_id = apartment.save()
+            print(self_id)
+            return JsonResponse(data, status=200)
     else:
         return JsonResponse({'POST': 'load_data'}, status=400)
 
